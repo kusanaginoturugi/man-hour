ed8d9e07f2eb0701c2da1b0a0e3c54b0df00371b
Takuro onoue <onoue@showway.biz>
2015-03-07 10:29:02 +0900

    fix work_minutes bug

--- app/models/job.rb
+++ app/models/job.rb
@@ -8,14 +8,14 @@ class Job < ActiveRecord::Base
   scope :monthly, ->(begin_date) { where("begin_date > ? and end_date < ?", begin_date.beginning_of_month, begin_date.end_of_month) }
   scope :summery, ->(begin_date) { where("begin_date > ? and end_date < ?", begin_date.beginning_of_month, begin_date.end_of_month) }
 
+  after_find do
+    self.work_minutes = (self.end_date - self.begin_date) / 60
+  end
+
   before_save do
     if self.begin_date.present? and self.work_minutes.present? then
       self.end_date = self.begin_date + self.work_minutes.to_i * 60
     end
-    if self.outside_budget then
-      self.worktime = 0
-    else
-      self.worktime = (self.end_date - self.begin_date) / 60 / 60
-    end
+    self.worktime = (self.end_date - self.begin_date) / 60 / 60
   end
 end
