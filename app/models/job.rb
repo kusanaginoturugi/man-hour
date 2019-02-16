2ad77e5e0868875400eb981ad95b444b504ca975
onoue <onoue@showway.biz>
2019-02-16 16:38:55 +0900

    monthly_summary bug fix

--- app/models/job.rb
+++ app/models/job.rb
@@ -25,24 +25,29 @@ class Job < ApplicationRecord
   end
 
   def summary_save
-    c_id = self.customer_id
-    date = self.begin_date
-    year       = date.year
-    month      = date.month
+    w_hours   = 18
+    c_id  = self.customer_id
+    date  = self.begin_date
+    year  = date.year
+    month = date.month
+
+    begin_at   = date.beginning_of_month.beginning_of_day
+    end_at     = date.end_of_month.end_of_day
+    amount     = Job.where(customer_id: c_id, begin_date: begin_at..end_at, outside_budget: false).sum(:worktime)
 
     prev_date  = date.prev_month
     prev_year  = prev_date.year
     prev_month = prev_date.month
 
-    begin_at   = date.beginning_of_month.beginning_of_day
-    end_at     = date.end_of_month.end_of_day
-    amount     = Job.where(customer_id: c_id, begin_date: begin_at..end_at, outside_budget: false).sum(:worktime)
+    prev_begin_at = prev_date.beginning_of_month.beginning_of_day
+    prev_end_at   = prev_date.end_of_month.end_of_day
+    prev_amount   = Job.where(customer_id: c_id, begin_date: prev_begin_at..prev_end_at, outside_budget: false).sum(:worktime)
+
     loop do
-      w_hours   = 18
       msum      = MonthlySummary.where(customer_id: c_id, year: year, month: month).first
       msum_prev = MonthlySummary.where(customer_id: c_id, year: prev_year, month: prev_month).first
       if msum.blank? and msum_prev.blank?
-        MonthlySummary.create(customer_id: c_id, year: year, month: month, begin_at: begin_at, end_at: end_at, carryover_amount: 0, this_month_amount: amount, amount: amount - w_hours)
+        MonthlySummary.create(customer_id: c_id, year: prev_year, month: prev_month, begin_at: prev_begin_at, end_at: prev_end_at, carryover_amount: 0, this_month_amount: prev_amount, amount: prev_amount - w_hours)
       elsif msum.blank? and msum_prev.present?
         MonthlySummary.create(customer_id: c_id, year: year, month: month, begin_at: begin_at, end_at: end_at, carryover_amount: msum_prev.amount, this_month_amount: amount, amount: msum_prev.amount + amount - w_hours)
         break
@@ -58,5 +63,6 @@ class Job < ApplicationRecord
         break
       end
     end
+
   end
 end
