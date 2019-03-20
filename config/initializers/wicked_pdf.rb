065fd8c9862991f7b1e5027c9569091e8ffd2b5b
onoue <onoue@showway.biz>
2019-03-20 14:24:01 +0900

    wkhtmltopdfのパスの修正(2)

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,5 @@
 if Rails.env.staging? || Rails.env.production?
-  WickedPdf.config = { :exe_path => Rails.root.join('app/vendor/bundle/bin', 'wkhtmltopdf').to_s}
+  WickedPdf.config = { :exe_path => Rails.root.join('vendor/bundle/bin', 'wkhtmltopdf').to_s}
 else
   WickedPdf.config = { :exe_path => '/usr/bin/wkhtmltopdf'}
 end
